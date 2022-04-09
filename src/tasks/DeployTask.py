0ae208051a6fc8af3e05b0984f79e684a945d60b
Caio Ueno <caioluiggy@hotmail.com>
2022-04-09 16:03:15 -0300

    finish luigi tasks

---
+++ src/tasks/DeployTask.py
@@ -0,0 +1,32 @@
+import logging
+
+import luigi
+
+from src.tasks.EvaluationTask import EvaluationTask
+
+logger = logging.getLogger(__name__)
+logger.setLevel("INFO")
+
+logging.basicConfig(
+    filename="automated-eval.log",
+    filemode="w",
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level="INFO",
+)
+
+
+class DeployTask(luigi.Task):
+
+    in_file = luigi.Parameter()
+
+    def complete(self):
+        return False
+
+    def requires(self):
+        return EvaluationTask(in_file=self.in_file)
+
+    def run(self):
+
+        logger.info(f"Start Deploy Task.")
+
+        # fake deploy
--- src/tasks/PreProcessTask.py
+++ src/tasks/PreProcessTask.py
@@ -21,24 +21,16 @@ class PreProcessTask(luigi.Task):
         logger.info(f"Read provided data: {self.in_file}")
         data = pd.read_csv(f"{self.in_file}")
 
-        # sepal_length,sepal_width,petal_length,petal_width,species
-
         X = data[["sepal_length", "sepal_width", "petal_length", "petal_width"]].copy()
         y = data["species"].copy()
 
         logger.info(f"Scale features.")
-        # scale features
         scaler = StandardScaler()
         X_preprocessed = scaler.fit_transform(X)
 
         logger.info(f"Encode string labels to integers.")
-        # transform string labels to integers
         encoder = LabelEncoder()
         y_preprocessed = encoder.fit_transform(y)
-
-        # # one hot encode labels
-        # enc = OneHotEncoder()
-        # y_preprocessed = enc.fit_transform(y.values)
 
         # store as a dataframe
         df = pd.DataFrame(
