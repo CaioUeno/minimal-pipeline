7254f2caac88a8f8ed0f2bf966de4f053fdde39e
Caio Ueno <caioluiggy@hotmail.com>
2022-04-10 12:07:21 -0300

    add data for testing

--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -11,7 +11,7 @@ from src.models.NearestNeighbors import NearestNeighbors
 
 @pytest.fixture
 def data():
-    return pd.read_csv("data/preprocessed.csv")
+    return pd.read_csv("tests/preprocessed.csv")
 
 
 @pytest.fixture
